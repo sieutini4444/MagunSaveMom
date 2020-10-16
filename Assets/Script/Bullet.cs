77ccbf50744a902a3c9382328e74899851f9a2b6
sieutini4444 <sieutini4444@gmail.com>
2020-10-16 14:05:19 +0700

    Create MagunSaveMom

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 0.3f;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+}
+
+    
---
+++ Assets/Script/Falling.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling : MonoBehaviour
+{
+    public Rigidbody2D rbody;
+    public float timedelay;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            StartCoroutine(fall());
+        }
+    }
+
+    IEnumerator fall()
+    {
+        yield return new WaitForSeconds(timedelay);
+        rbody.bodyType = RigidbodyType2D.Dynamic;
+        yield return 0;
+
+    }
+
+}
