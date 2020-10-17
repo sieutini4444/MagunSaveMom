bd7321c9d1376a86904ea839bdc0d86977b2d800
sieutini4444 <sieutini4444@gmail.com>
2020-10-17 10:21:29 +0700

    Make Ninja + Fix Map

---
+++ Assets/Mover.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed = 40f, maxspeed = 3, jumpPow = 300f;
+    public bool grounded = true, faceright = true, doublejump = false;
+
+    public GameObject playerr;
+    public Rigidbody2D r2;
+    public Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        r2 = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            playerr.transform.position = (new Vector2(-5, 0));
+        }
+        anim.SetBool("Ground", grounded);
+        anim.SetFloat("speed", Mathf.Abs(r2.velocity.x));
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (grounded)
+            {
+                grounded = false;
+                doublejump = true;
+                r2.AddForce(Vector2.up * jumpPow);         
+            }
+            else
+            {
+                if (doublejump)
+                {
+                    doublejump = false;
+                    r2.velocity = new Vector2(r2.velocity.x, 0);
+                    r2.AddForce(Vector2.up * jumpPow * 0.8f);
+                }
+            }
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        r2.AddForce((Vector2.right) * speed * h);
+
+        if (r2.velocity.x > maxspeed)
+            r2.velocity = new Vector2(maxspeed, r2.velocity.y);
+        if (r2.velocity.x < -maxspeed)
+            r2.velocity = new Vector2(-maxspeed, r2.velocity.y);
+
+        if (h > 0 && !faceright)
+        {
+            Flip();
+        }
+
+        if (h < 0 && faceright)
+        {
+            Flip();
+        }
+    }
+
+    public void Flip()
+    {
+        faceright = !faceright;
+        Vector3 Scale;
+        Scale = transform.localScale;
+        Scale.x *= -1;
+        transform.localScale = Scale;
+    }
+}
